7b8bab4057df39f9b24294a9efa761d920905601
dkdace <darkdace2@naver.com>
2025-07-30 23:40:15 +0900

    침대를 외부 차원에서 사용할 수 없도록 수정

--- src/main/java/com/dace/vanillaplus/VanillaPlus.java
+++ src/main/java/com/dace/vanillaplus/VanillaPlus.java
@@ -5,6 +5,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+/**
+ * 모드 메인 클래스.
+ */
 @Mod(VanillaPlus.MODID)
 public final class VanillaPlus {
     public static final String MODID = "vanillaplus";
---
+++ src/main/java/com/dace/vanillaplus/mixin/BedBlockMixin.java
@@ -0,0 +1,33 @@
+package com.dace.vanillaplus.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BedBlock.class)
+public final class BedBlockMixin {
+    @Overwrite
+    public static boolean canSetSpawn(Level level) {
+        return true;
+    }
+
+    @Inject(method = "useWithoutItem", at = @At("RETURN"), cancellable = true)
+    private void useWithoutItem(BlockState blockState, Level level, BlockPos blockPos, Player player, BlockHitResult blockHitResult,
+                                CallbackInfoReturnable<InteractionResult> cir) {
+        if (level.dimension() == Level.OVERWORLD)
+            return;
+
+        player.displayClientMessage(Component.translatable("block.vanillaplus.bed.no_use"), true);
+        cir.setReturnValue(InteractionResult.SUCCESS_SERVER);
+    }
+}
