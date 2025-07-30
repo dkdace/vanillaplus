bd9360f9df761373871e040c40dd3f695510d02c
dkdace <darkdace2@naver.com>
2025-07-30 23:50:16 +0900

    어려움 난이도에서 몹이 공격적 상태에서 탈것에 탑승할 수 없도록 수정

---
+++ src/main/java/com/dace/vanillaplus/mixin/MobMixin.java
@@ -0,0 +1,30 @@
+package com.dace.vanillaplus.mixin;
+
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.vehicle.VehicleEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Mob.class)
+public final class MobMixin {
+    @Inject(method = "setAggressive", at = @At("HEAD"))
+    private void setAggressive(boolean isAggressive, CallbackInfo ci) {
+        Mob mob = (Mob) (Object) this;
+
+        if (mob.level().getDifficulty() == Difficulty.HARD && isAggressive && mob.getVehicle() instanceof VehicleEntity)
+            mob.stopRiding();
+    }
+
+    @Inject(method = "startRiding", at = @At("HEAD"), cancellable = true)
+    private void startRiding(Entity vehicle, boolean force, CallbackInfoReturnable<Boolean> cir) {
+        Mob mob = (Mob) (Object) this;
+
+        if (mob.level().getDifficulty() == Difficulty.HARD && mob.isAggressive() && vehicle instanceof VehicleEntity)
+            cir.setReturnValue(false);
+    }
+}
