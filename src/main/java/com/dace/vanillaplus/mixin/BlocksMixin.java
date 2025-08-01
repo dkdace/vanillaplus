eed268305c06067373e1ea0242680aca80dc5ba7
dkdace <darkdace2@naver.com>
2025-08-02 03:09:45 +0900

    석영 채굴로 얻는 경험치 감소

---
+++ src/main/java/com/dace/vanillaplus/mixin/BlocksMixin.java
@@ -0,0 +1,17 @@
+package com.dace.vanillaplus.mixin;
+
+import net.minecraft.world.level.block.Blocks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(Blocks.class)
+public final class BlocksMixin {
+    @ModifyArgs(method = "lambda$static$224", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/util/valueproviders/UniformInt;of(II)Lnet/minecraft/util/valueproviders/UniformInt;"))
+    private static void getXP(Args args) {
+        args.set(0, 1);
+        args.set(1, 3);
+    }
+}
