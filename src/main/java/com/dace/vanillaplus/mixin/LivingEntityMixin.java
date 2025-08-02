4928b29f9711a7a0848b763910687478b5a14e45
dkdace <darkdace06@gmail.com>
2025-08-03 03:34:07 +0900

    몹이 투명한 블록 너머로 플레이어를 감지하도록 수정

---
+++ src/main/java/com/dace/vanillaplus/mixin/LivingEntityMixin.java
@@ -0,0 +1,17 @@
+package com.dace.vanillaplus.mixin;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.ClipContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(LivingEntity.class)
+public final class LivingEntityMixin {
+    @ModifyArg(method = "hasLineOfSight(Lnet/minecraft/world/entity/Entity;)Z", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/LivingEntity;hasLineOfSight(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/ClipContext$Block;Lnet/minecraft/world/level/ClipContext$Fluid;D)Z"),
+            index = 1)
+    private ClipContext.Block modifyLineOfSightClipContextBlock(ClipContext.Block block) {
+        return ClipContext.Block.VISUAL;
+    }
+}
