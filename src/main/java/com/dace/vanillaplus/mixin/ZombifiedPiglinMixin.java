92643e741bdba9fb79f831b6f11be957ff673cd0
dkdace <darkdace2@naver.com>
2025-08-10 21:36:53 +0900

    좀비화 피글린을 일격에 처치해도 주변 좀비화 피글린을 공격적으로 만들도록 수정

---
+++ src/main/java/com/dace/vanillaplus/mixin/ZombifiedPiglinMixin.java
@@ -0,0 +1,29 @@
+package com.dace.vanillaplus.mixin;
+
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.ZombifiedPiglin;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ZombifiedPiglin.class)
+public abstract class ZombifiedPiglinMixin extends LivingEntityMixin {
+    @Shadow
+    public abstract void setTarget(@Nullable LivingEntity p_34478_);
+
+    @Shadow
+    protected abstract void alertOthers();
+
+    @Override
+    protected void onDie(DamageSource damageSource, CallbackInfo ci) {
+        Entity entity = damageSource.getEntity();
+        if (!(entity instanceof LivingEntity))
+            return;
+
+        setTarget((LivingEntity) entity);
+        alertOthers();
+    }
+}
