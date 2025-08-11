541ae7fa0587049c1f97332d9884ec8dcb557fcd
dkdace <darkdace2@naver.com>
2025-08-11 19:04:44 +0900

    침대 사용 방지 로직 수정

--- src/main/java/com/dace/vanillaplus/mixin/BedBlockMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/BedBlockMixin.java
@@ -9,24 +9,16 @@ import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BedBlock.class)
 public final class BedBlockMixin {
-    @Overwrite
-    public static boolean canSetSpawn(Level level) {
-        return true;
-    }
-
-    @Inject(method = "useWithoutItem", at = @At("RETURN"), cancellable = true)
-    private void preventUseInOtherDimension(BlockState blockState, Level level, BlockPos blockPos, Player player,
-                                            BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
-        if (level.dimension() == Level.OVERWORLD)
-            return;
-
+    @Inject(method = "useWithoutItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;removeBlock(Lnet/minecraft/core/BlockPos;Z)Z",
+            ordinal = 0), cancellable = true)
+    private void preventUseIfCannotUse(BlockState blockState, Level level, BlockPos blockPos, Player player, BlockHitResult blockHitResult,
+                                       CallbackInfoReturnable<InteractionResult> cir) {
         player.displayClientMessage(Component.translatable("block.vanillaplus.bed.no_use"), true);
         cir.setReturnValue(InteractionResult.SUCCESS_SERVER);
     }
