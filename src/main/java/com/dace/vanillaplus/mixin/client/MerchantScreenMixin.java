e3d9b3a6677106a488cfdf58b9f08d911765d019
dkdace <darkdace2@naver.com>
2025-08-19 17:43:34 +0900

    주민 거래 GUI 재고 표시 수정

--- src/main/java/com/dace/vanillaplus/mixin/client/MerchantScreenMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/client/MerchantScreenMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MerchantScreen.class)
@@ -23,28 +24,36 @@ public abstract class MerchantScreenMixin extends AbstractContainerScreenMixin {
     @Final
     private static int SELL_ITEM_2_X;
 
+    @ModifyArg(method = "render", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/screens/inventory/MerchantScreen;renderButtonArrows(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/world/item/trading/MerchantOffer;II)V"),
+            index = 3)
+    private int modifyArrowIconY(int y) {
+        return y + 6;
+    }
+
     @Inject(method = "render", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/client/gui/screens/inventory/MerchantScreen;renderButtonArrows(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/world/item/trading/MerchantOffer;II)V"))
-    private void renderStockTooltip(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci, @Local(ordinal = 2) int x,
-                                    @Local(ordinal = 7) int y, @Local MerchantOffer merchantOffer) {
+    private void renderStock(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci, @Local(ordinal = 2) int x,
+                             @Local(ordinal = 7) int y, @Local MerchantOffer merchantOffer) {
         String remainingUses = Integer.toString(merchantOffer.getMaxUses() - merchantOffer.getUses());
         String maxUses = Integer.toString(merchantOffer.getMaxUses());
 
         Component stockComponent = Component.literal(remainingUses);
-        int stockTextWidth = font.width(stockComponent);
-        int stockTextX = x + SELL_ITEM_1_X + SELL_ITEM_2_X + 18 - stockTextWidth;
-        int stockTextY = y + 4;
+        int stockTextX = x + SELL_ITEM_1_X + SELL_ITEM_2_X + 25;
+        int stockTextY = y + 1;
         int stockTextColor;
         if (merchantOffer.isOutOfStock())
             stockTextColor = ARGB.color(0xff, 0x60, 0x60);
         else
             stockTextColor = remainingUses.equals(maxUses)
-                    ? ARGB.color(0x20, 0xb0, 0xff)
-                    : ARGB.color(0x80, 0xff, 0x20);
+                    ? ARGB.color(0x80, 0xff, 0x20)
+                    : ARGB.color(0xff, 0xff, 0xff);
 
-        guiGraphics.drawString(font, stockComponent, stockTextX, stockTextY, stockTextColor, false);
+        guiGraphics.drawCenteredString(font, stockComponent, stockTextX, stockTextY, stockTextColor);
 
-        if (!isHovering(stockTextX - leftPos, stockTextY - topPos, stockTextWidth, font.lineHeight, mouseX, mouseY))
+        int hoverX = x + SELL_ITEM_1_X + SELL_ITEM_2_X + 18 - leftPos;
+        int hoverY = y - topPos;
+        if (!isHovering(hoverX, hoverY, 12, 18, mouseX, mouseY))
             return;
 
         MutableComponent tooltipComponent = Component.translatable("merchant.stock", remainingUses, maxUses);
