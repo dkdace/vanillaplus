a013da5281cbfef490595a828d1e54201d6c0a3c
dkdace <darkdace06@gmail.com>
2025-08-21 17:18:38 +0900

    불사의 토템 쿨타임 추가, 상태 효과 지속시간 감소

--- src/main/java/com/dace/vanillaplus/mixin/LivingEntityMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/LivingEntityMixin.java
@@ -1,9 +1,15 @@
 package com.dace.vanillaplus.mixin;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.DeathProtection;
 import net.minecraft.world.level.ClipContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,6 +17,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends EntityMixin {
@@ -32,6 +39,18 @@ public abstract class LivingEntityMixin extends EntityMixin {
 
     @Inject(method = "die", at = @At("TAIL"))
     protected void onDie(DamageSource damageSource, CallbackInfo ci) {
+        // 미사용
+    }
+
+    @Definition(id = "deathprotection", local = @Local(type = DeathProtection.class))
+    @Expression("deathprotection != null")
+    @ModifyExpressionValue(method = "checkTotemDeathProtection", at = @At(value = "MIXINEXTRAS:EXPRESSION", ordinal = 0))
+    protected boolean canUseTotem(boolean canUse, @Local(ordinal = 1) ItemStack itemStack) {
+        return canUse;
+    }
+
+    @Inject(method = "checkTotemDeathProtection", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;setHealth(F)V"))
+    protected void onUseTotem(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir, @Local ItemStack itemStack) {
         // 미사용
     }
 }
--- src/main/java/com/dace/vanillaplus/mixin/PlayerMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/PlayerMixin.java
@@ -1,21 +1,29 @@
 package com.dace.vanillaplus.mixin;
 
 import com.dace.vanillaplus.custom.CustomPlayer;
+import com.dace.vanillaplus.rebalance.Rebalance;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Abilities;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemCooldowns;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends LivingEntityMixin implements CustomPlayer {
     @Shadow
     @Final
     private Abilities abilities;
+    @Shadow
+    @Final
+    private ItemCooldowns cooldowns;
     @Unique
     private boolean vp$isProneKeyDown = false;
 
@@ -27,5 +35,15 @@ public abstract class PlayerMixin extends LivingEntityMixin implements CustomPla
     @ModifyReturnValue(method = "getDesiredPose", at = @At(value = "RETURN", ordinal = 4))
     private Pose modifyDesiredPose(Pose pose) {
         return vp$isProneKeyDown && !abilities.flying && onGround() ? Pose.SWIMMING : pose;
+    }
+
+    @Override
+    protected boolean canUseTotem(boolean canUse, ItemStack itemStack) {
+        return canUse && !cooldowns.isOnCooldown(itemStack);
+    }
+
+    @Override
+    protected void onUseTotem(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir, ItemStack itemStack) {
+        cooldowns.addCooldown(itemStack, Rebalance.Totem.COOLDOWN_SECONDS * 20);
     }
 }
