4eff9bb657de1cf8b33923dbc9da0a36ed113cca
dkdace <darkdace2@naver.com>
2025-10-03 23:24:31 +0900

    행운 인첸트의 광물 채굴 계산식 수정, 일부 블록의 적용 방식 변경

---
+++ src/main/java/com/dace/vanillaplus/mixin/world/level/storage/loot/functions/ApplyBonusCountMixin.java
@@ -0,0 +1,21 @@
+package com.dace.vanillaplus.mixin.world.level.storage.loot.functions;
+
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(ApplyBonusCount.class)
+public abstract class ApplyBonusCountMixin {
+    @Mixin(targets = {"net.minecraft.world.level.storage.loot.functions.ApplyBonusCount$OreDrops"})
+    public abstract static class OreDropsMixin {
+        @Overwrite
+        public int calculateNewCount(RandomSource randomSource, int count, int level) {
+            if (level <= 0)
+                return count;
+
+            double chance = level * 0.3;
+            return chance > randomSource.nextFloat() ? count + 1 : count;
+        }
+    }
+}
