1a280e43d03add06f90591ee3df37d47e3a0a626
dkdace <darkdace2@naver.com>
2025-10-11 16:48:16 +0900

    제작 및 숫돌로 수리 시 수리 한도 초기화

---
+++ src/main/java/com/dace/vanillaplus/mixin/world/inventory/GrindstoneMenuMixin.java
@@ -0,0 +1,19 @@
+package com.dace.vanillaplus.mixin.world.inventory;
+
+import com.dace.vanillaplus.extension.VPItemStack;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.inventory.GrindstoneMenu;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(GrindstoneMenu.class)
+public abstract class GrindstoneMenuMixin {
+    @Inject(method = "mergeItems", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;setDamageValue(I)V"))
+    private void resetRepairLimit(ItemStack topItemStack, ItemStack bottomItemStack, CallbackInfoReturnable<ItemStack> cir,
+                                  @Local(ordinal = 2) ItemStack itemStack) {
+        VPItemStack.setRepairLimit(itemStack, 0);
+    }
+}
