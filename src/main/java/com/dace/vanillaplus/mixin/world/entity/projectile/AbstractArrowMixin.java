071978d041b6e47375cdf209b4b0c13decaa6fa0
dkdace <darkdace2@naver.com>
2025-10-30 19:40:11 +0900

    화살이 플레이어가 아닌 몹에 명중했을 때도 효과음이 재생되도록 수정

--- src/main/java/com/dace/vanillaplus/mixin/world/entity/projectile/AbstractArrowMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/world/entity/projectile/AbstractArrowMixin.java
@@ -3,10 +3,13 @@ package com.dace.vanillaplus.mixin.world.entity.projectile;
 import com.dace.vanillaplus.data.modifier.EntityModifier;
 import com.dace.vanillaplus.mixin.world.entity.EntityMixin;
 import com.dace.vanillaplus.registryobject.VPAttributes;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,5 +21,13 @@ public abstract class AbstractArrowMixin<T extends AbstractArrow, U extends Enti
     private double modifyKnockbackStrength(double original, @Local(argsOnly = true) LivingEntity livingEntity,
                                            @Local(argsOnly = true) DamageSource damageSource) {
         return VPAttributes.getFinalKnockbackResistance(livingEntity, damageSource);
+    }
+
+    @Definition(id = "livingentity", local = @Local(type = LivingEntity.class))
+    @Definition(id = "Player", type = Player.class)
+    @Expression("livingentity instanceof Player")
+    @ModifyExpressionValue(method = "onHitEntity", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private boolean modifyHitSoundCondition(boolean original) {
+        return true;
     }
 }
