932642628ce083922b277f638201860d751aa00b
dkdace <darkdace2@naver.com>
2025-11-01 19:40:04 +0900

    장비 녹이기를 구리 아이템에도 적용, nuggets 아이템 데이터 태그 추가

--- src/main/java/com/dace/vanillaplus/data/modifier/GeneralModifier.java
+++ src/main/java/com/dace/vanillaplus/data/modifier/GeneralModifier.java
@@ -31,7 +31,7 @@ public final class GeneralModifier {
     /** 전역 수정자 리소스 이름 */
     private static final String RESOURCE_NAME = "general";
 
-    /** 철 또는 금 도구를 화로에서 녹일 때 내구도 감소 비율 */
+    /** 구리, 철 또는 금 도구를 화로에서 녹일 때 내구도 감소 비율 */
     private final float smeltingToolDamageRatio;
     /** 최대 내구도 : 최대 수리 한도 비율 */
     private final float maxRepairLimitRatio;
