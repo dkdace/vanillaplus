f2e529dbea76f0850ffe34af3aed7ce9d7c0addf
dkdace <darkdace2@naver.com>
2025-12-01 22:22:46 +0900

    nuggets 아이템 데이터 태그 제거 (Forge 내장)

--- src/main/java/com/dace/vanillaplus/mixin/world/level/block/entity/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/world/level/block/entity/AbstractFurnaceBlockEntityMixin.java
@@ -1,6 +1,5 @@
 package com.dace.vanillaplus.mixin.world.level.block.entity;
 
-import com.dace.vanillaplus.VPTags;
 import com.dace.vanillaplus.data.GeneralConfig;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.NonNullList;
@@ -11,6 +10,7 @@ import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.SingleRecipeInput;
 import net.minecraft.world.item.enchantment.EnchantmentEffectComponents;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
+import net.minecraftforge.common.Tags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,7 +22,7 @@ public abstract class AbstractFurnaceBlockEntityMixin<T extends AbstractFurnaceB
     private void damageBurnedItem(RegistryAccess registryAccess, RecipeHolder<? extends AbstractCookingRecipe> recipeHolder,
                                   SingleRecipeInput recipeInput, NonNullList<ItemStack> itemStacks, int maxStackSize,
                                   CallbackInfoReturnable<Boolean> cir, @Local(ordinal = 0) ItemStack input, @Local(ordinal = 1) ItemStack output) {
-        if (!input.isDamageableItem() || !output.is(VPTags.Items.NUGGETS))
+        if (!input.isDamageableItem() || !output.is(Tags.Items.NUGGETS))
             return;
         if (input.getEnchantments().keySet().stream().anyMatch(enchantmentHolder ->
                 enchantmentHolder.value().effects().has(EnchantmentEffectComponents.REPAIR_WITH_XP)))
