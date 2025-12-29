265cfc21d005722bd976914be574e271cbed6006
dkdace <darkdace2@naver.com>
2025-12-29 23:50:01 +0900

    마법 부여 확장(VPEnchantment) 메소드의 주석 수정

--- src/main/java/com/dace/vanillaplus/extension/world/item/enchantment/VPEnchantment.java
+++ src/main/java/com/dace/vanillaplus/extension/world/item/enchantment/VPEnchantment.java
@@ -1,10 +1,9 @@
 package com.dace.vanillaplus.extension.world.item.enchantment;
 
 import com.dace.vanillaplus.extension.VPMixin;
+import com.dace.vanillaplus.registryobject.VPEnchantmentEffectComponentTypes;
 import lombok.NonNull;
-import net.minecraft.core.component.DataComponentType;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
@@ -21,31 +20,66 @@ public interface VPEnchantment extends VPMixin<Enchantment> {
     }
 
     /**
-     * @see Enchantment#modifyEntityFilteredValue(DataComponentType, ServerLevel, int, ItemStack, Entity, MutableFloat)
+     * 획득 경험치 배수 수치를 적용한다.
+     *
+     * @param serverLevel      월드
+     * @param enchantmentLevel 마법 부여 레벨
+     * @param itemStack        대상 아이템
+     * @param entity           대상 엔티티
+     * @param multiplier       경험치 배수 값
+     * @see VPEnchantmentEffectComponentTypes#XP_MULTIPLIER
      */
     void modifyXpMultiplier(@NonNull ServerLevel serverLevel, int enchantmentLevel, @NonNull ItemStack itemStack, @NonNull Entity entity,
                             @NonNull MutableFloat multiplier);
 
     /**
-     * @see Enchantment#modifyUnfilteredValue(DataComponentType, RandomSource, int, MutableFloat)
+     * 철 골렘 회복 배수 수치를 적용한다.
+     *
+     * @param enchantmentLevel 마법 부여 레벨
+     * @param itemStack        대상 아이템
+     * @param entity           대상 엔티티
+     * @param healMultiplier   회복 배수 값
+     * @see VPEnchantmentEffectComponentTypes#IRON_GOLEM_HEAL_MULTIPLIER
      */
     void modifyIronGolemHealMultiplier(int enchantmentLevel, @NonNull ItemStack itemStack, @NonNull Entity entity,
                                        @NonNull MutableFloat healMultiplier);
 
     /**
-     * @see Enchantment#modifyEntityFilteredValue(DataComponentType, ServerLevel, int, ItemStack, Entity, MutableFloat)
+     * 피글린 거래 횟수 수치를 적용한다.
+     *
+     * @param serverLevel      월드
+     * @param enchantmentLevel 마법 부여 레벨
+     * @param itemStack        대상 아이템
+     * @param entity           대상 엔티티
+     * @param rolls            거래 횟수 값
+     * @see VPEnchantmentEffectComponentTypes#BARTERING_ROLLS
      */
     void modifyBarteringRolls(@NonNull ServerLevel serverLevel, int enchantmentLevel, @NonNull ItemStack itemStack, @NonNull Entity entity,
                               @NonNull MutableFloat rolls);
 
     /**
-     * @see Enchantment#modifyEntityFilteredValue(DataComponentType, ServerLevel, int, ItemStack, Entity, MutableFloat)
+     * 주민 거래 가격 배수 수치를 적용한다.
+     *
+     * @param serverLevel      월드
+     * @param enchantmentLevel 마법 부여 레벨
+     * @param itemStack        대상 아이템
+     * @param entity           대상 엔티티
+     * @param multiplier       가격 배수 값
+     * @see VPEnchantmentEffectComponentTypes#TRADING_COST_MULTIPLIER
      */
     void modifyTradingCostMultiplier(@NonNull ServerLevel serverLevel, int enchantmentLevel, @NonNull ItemStack itemStack, @NonNull Entity entity,
                                      @NonNull MutableFloat multiplier);
 
     /**
-     * @see Enchantment#modifyDamageProtection(ServerLevel, int, ItemStack, Entity, DamageSource, MutableFloat)
+     * 최종 피해량 배수 수치를 적용한다.
+     *
+     * @param serverLevel      월드
+     * @param enchantmentLevel 마법 부여 레벨
+     * @param itemStack        대상 아이템
+     * @param entity           대상 엔티티
+     * @param damageSource     피해 근원
+     * @param multiplier       피해량 배수 값
+     * @see VPEnchantmentEffectComponentTypes#FINAL_INCOMING_DAMAGE_MULTIPLIER
      */
     void modifyFinalIncomingDamageMultiplier(@NonNull ServerLevel serverLevel, int enchantmentLevel, @NonNull ItemStack itemStack,
                                              @NonNull Entity entity, @NonNull DamageSource damageSource, @NonNull MutableFloat multiplier);
