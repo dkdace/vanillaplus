97a72de0a08577324eb78d74abdb4746bd53ec39
dkdace <darkdace2@naver.com>
2026-01-06 16:43:06 +0900

    철 골렘을 철 조각으로 1/9만큼 수리할 수 있도록 수정

--- src/main/java/com/dace/vanillaplus/mixin/world/entity/animal/IronGolemMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/world/entity/animal/IronGolemMixin.java
@@ -3,9 +3,12 @@ package com.dace.vanillaplus.mixin.world.entity.animal;
 import com.dace.vanillaplus.data.modifier.EntityModifier;
 import com.dace.vanillaplus.extension.world.item.enchantment.VPEnchantment;
 import com.dace.vanillaplus.mixin.world.entity.MobMixin;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.entity.animal.IronGolem;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.phys.AABB;
 import org.apache.commons.lang3.mutable.MutableFloat;
@@ -20,12 +23,21 @@ public abstract class IronGolemMixin extends MobMixin<IronGolem, EntityModifier.
         return aabb.inflate(0.5, 0.1, 0.5);
     }
 
+    @ModifyExpressionValue(method = "mobInteract", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
+    private boolean modifyHealItems(boolean canHeal, @Local ItemStack itemStack) {
+        return canHeal || itemStack.is(Items.IRON_NUGGET);
+    }
+
     @ModifyArg(method = "mobInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/animal/IronGolem;heal(F)V"))
-    private float modifyHealAmount(float amount, @Local(argsOnly = true) Player player) {
+    private float modifyHealAmount(float amount, @Local(argsOnly = true) Player player, @Local ItemStack itemStack) {
         MutableFloat value = new MutableFloat(1);
 
         EnchantmentHelper.runIterationOnEquipment(player, (enchantmentHolder, level, enchantedItemInUse) ->
                 VPEnchantment.cast(enchantmentHolder.value()).modifyIronGolemHealMultiplier(level, player, value));
+
+        if (itemStack.is(Items.IRON_NUGGET))
+            amount /= 9;
 
         return amount * value.floatValue();
     }
