f9801325185f9d0ac8b08e6b9d715346ff849e3f
dkdace <darkdace2@naver.com>
2026-01-06 22:04:43 +0900

    반짝이는 수박 조각, 설탕, 튀긴 후렴과를 먹을 수 있도록 수정

--- src/main/java/com/dace/vanillaplus/mixin/world/item/CreativeModeTabsMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/world/item/CreativeModeTabsMixin.java
@@ -3,14 +3,39 @@ package com.dace.vanillaplus.mixin.world.item;
 import com.dace.vanillaplus.data.GeneralConfig;
 import com.dace.vanillaplus.extension.VPMixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CreativeModeTabs.class)
 public abstract class CreativeModeTabsMixin implements VPMixin<CreativeModeTabs> {
     @ModifyExpressionValue(method = "generateOminousBottles", at = @At(value = "CONSTANT", args = "intValue=4"))
     private static int modifyOminousBottleCount(int original) {
         return GeneralConfig.get().getMaxBadOmenLevel() - 1;
+    }
+
+    @Inject(method = "lambda$bootstrap$23", at = @At(value = "FIELD",
+            target = "Lnet/minecraft/world/item/Items;SWEET_BERRIES:Lnet/minecraft/world/item/Item;", opcode = Opcodes.GETSTATIC))
+    private static void addGoldenCarrotToFoods(CreativeModeTab.ItemDisplayParameters itemDisplayParameters, CreativeModeTab.Output output,
+                                               CallbackInfo ci) {
+        output.accept(Items.GLISTERING_MELON_SLICE);
+    }
+
+    @Inject(method = "lambda$bootstrap$23", at = @At(value = "FIELD",
+            target = "Lnet/minecraft/world/item/Items;COOKIE:Lnet/minecraft/world/item/Item;", opcode = Opcodes.GETSTATIC))
+    private static void addSugarToFoods(CreativeModeTab.ItemDisplayParameters itemDisplayParameters, CreativeModeTab.Output output, CallbackInfo ci) {
+        output.accept(Items.SUGAR);
+    }
+
+    @Inject(method = "lambda$bootstrap$23", at = @At(value = "FIELD",
+            target = "Lnet/minecraft/world/item/Items;CARROT:Lnet/minecraft/world/item/Item;", opcode = Opcodes.GETSTATIC))
+    private static void addPoppedChorusFruitToFoods(CreativeModeTab.ItemDisplayParameters itemDisplayParameters, CreativeModeTab.Output output,
+                                                    CallbackInfo ci) {
+        output.accept(Items.POPPED_CHORUS_FRUIT);
     }
 }
