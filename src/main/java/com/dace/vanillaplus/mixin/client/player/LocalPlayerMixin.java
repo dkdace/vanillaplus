d58a685b6c6ddf7698d7d9d57f88cbab173d9ced
dkdace <darkdace2@naver.com>
2026-01-28 23:42:48 +0900

    블록을 부수는 중 아이템을 버렸을 때 채굴 상태가 초기화되지 않는 버그 수정 (MC-165381)

--- src/main/java/com/dace/vanillaplus/mixin/client/player/LocalPlayerMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/client/player/LocalPlayerMixin.java
@@ -4,16 +4,24 @@ import com.dace.vanillaplus.mixin.world.entity.player.PlayerMixin;
 import com.dace.vanillaplus.registryobject.VPAttributes;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.InteractionHand;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LocalPlayer.class)
 public abstract class LocalPlayerMixin extends PlayerMixin<LocalPlayer> {
+    @Shadow
+    @Final
+    protected Minecraft minecraft;
+
     @Shadow
     public abstract boolean isMovingSlowly();
 
@@ -31,5 +39,12 @@ public abstract class LocalPlayerMixin extends PlayerMixin<LocalPlayer> {
     @Inject(method = "swing", at = @At("HEAD"))
     public void resetAttackStrengthOnSwing(InteractionHand interactionHand, CallbackInfo ci) {
         attackStrengthTicker = 0;
+    }
+
+    @Inject(method = "drop", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;removeFromSelected(Z)Lnet/minecraft/world/item/ItemStack;", shift = At.Shift.AFTER))
+    private void stopDestroyingBlockOnDrop(boolean isFullStack, CallbackInfoReturnable<Boolean> cir) {
+        MultiPlayerGameMode gameMode = minecraft.gameMode;
+        if (gameMode != null)
+            gameMode.stopDestroyBlock();
     }
 }
