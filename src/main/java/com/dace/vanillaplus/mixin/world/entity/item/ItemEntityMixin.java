9a329e0e92adaafefc48d46006ddddf20ca7646a
dkdace <darkdace2@naver.com>
2026-01-29 20:23:33 +0900

    갓 생성된 아이템이 번개에 의해 즉시 파괴되는 버그 수정 (MC-206922)

---
+++ src/main/java/com/dace/vanillaplus/mixin/world/entity/item/ItemEntityMixin.java
@@ -0,0 +1,21 @@
+package com.dace.vanillaplus.mixin.world.entity.item;
+
+import com.dace.vanillaplus.data.modifier.EntityModifier;
+import com.dace.vanillaplus.mixin.world.entity.EntityMixin;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.item.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(ItemEntity.class)
+public abstract class ItemEntityMixin extends EntityMixin<ItemEntity, EntityModifier> {
+    @Unique
+    private static final int LIGHTNING_IMMUNE_DURATION = 8;
+
+    @Override
+    public void thunderHit(ServerLevel serverLevel, LightningBolt lightningBolt) {
+        if (tickCount > LIGHTNING_IMMUNE_DURATION)
+            super.thunderHit(serverLevel, lightningBolt);
+    }
+}
