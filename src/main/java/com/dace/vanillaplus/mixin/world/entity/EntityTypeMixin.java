2d49137998cbd23b1bfbc1c89961dd9473268e6f
dkdace <darkdace06@gmail.com>
2026-08-15 20:13:17 +0900

    일부 엔티티가 이동했을 때 클라이언트에 반영되지 않는 버그 수정

--- src/main/java/com/dace/vanillaplus/mixin/world/entity/EntityTypeMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/world/entity/EntityTypeMixin.java
@@ -3,11 +3,13 @@ package com.dace.vanillaplus.mixin.world.entity;
 import com.dace.vanillaplus.data.VPDataComponentMap;
 import com.dace.vanillaplus.extension.world.entity.VPEntityType;
 import com.dace.vanillaplus.world.entity.EntityConfig;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import lombok.NonNull;
 import net.minecraft.world.entity.EntityType;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.Objects;
 
@@ -16,6 +18,11 @@ public abstract class EntityTypeMixin implements VPEntityType {
     @Unique
     @Nullable
     private EntityConfig config;
+
+    @ModifyExpressionValue(method = "<clinit>", at = @At(value = "CONSTANT", args = "intValue=2147483647"))
+    private static int modifyMaxUpdateInterval(int updateInterval) {
+        return 20;
+    }
 
     @Override
     @NonNull
