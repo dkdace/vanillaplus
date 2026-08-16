4b60094fb45799535b47b105270cb16c254c1c9e
dkdace <darkdace06@gmail.com>
2026-08-16 18:40:07 +0900

    공격 표식 코드 주입 위치 수정

--- src/main/java/com/dace/vanillaplus/mixin/client/gui/GuiMixin.java
+++ src/main/java/com/dace/vanillaplus/mixin/client/gui/GuiMixin.java
@@ -19,6 +19,7 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.GameType;
 import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -28,6 +29,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
 
 @Mixin(Gui.class)
 public abstract class GuiMixin implements VPGui {
@@ -150,10 +153,10 @@ public abstract class GuiMixin implements VPGui {
             recentDamageTick--;
     }
 
-    @Inject(method = "extractCrosshair", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/components/debug/DebugScreenEntryList;isCurrentlyEnabled(Lnet/minecraft/resources/Identifier;)Z"))
+    @Inject(method = "extractCrosshair", at = @At(value = "TAIL"))
     private void renderCrosshairHitmarker(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
-        if (!VPOptions.cast(minecraft.options).getAttackMarker().get() || recentDamageTick <= 0 || hitmarkerSprite == null)
+        if (Objects.requireNonNull(minecraft.gameMode).getPlayerMode() == GameType.SPECTATOR
+                || !VPOptions.cast(minecraft.options).getAttackMarker().get() || recentDamageTick <= 0 || hitmarkerSprite == null)
             return;
 
         int color = ARGB.multiplyAlpha(hitmarkerColor, (float) recentDamageTick / RECENT_DAMAGE_DURATION);
